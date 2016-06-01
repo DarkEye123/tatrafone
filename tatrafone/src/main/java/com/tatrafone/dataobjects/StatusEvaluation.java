f31ad2b1a138b4f66446e3a6a1360e27ea9ea42c
pv207 <>
2016-06-01 19:34:51 +0200

    {/tatrafone/src/main/java/com/tatrafone/dataobjects/StatusEvaluation.java}

--- tatrafone/src/main/java/com/tatrafone/dataobjects/StatusEvaluation.java
+++ tatrafone/src/main/java/com/tatrafone/dataobjects/StatusEvaluation.java
@@ -13,8 +13,40 @@ public class StatusEvaluation extends ClientRequest implements java.io.Serializa
 
    static final long serialVersionUID = 1L;
 
+   @org.kie.api.definition.type.Label(value = "Decision")
+   private java.lang.Boolean decision;
+   @org.kie.api.definition.type.Label(value = "Reason of the Decision")
+   private java.lang.String reasonOfDecision;
+
    public StatusEvaluation()
    {
+   }
+
+   public java.lang.Boolean getDecision()
+   {
+      return this.decision;
+   }
+
+   public void setDecision(java.lang.Boolean decision)
+   {
+      this.decision = decision;
+   }
+
+   public java.lang.String getReasonOfDecision()
+   {
+      return this.reasonOfDecision;
+   }
+
+   public void setReasonOfDecision(java.lang.String reasonOfDecision)
+   {
+      this.reasonOfDecision = reasonOfDecision;
+   }
+
+   public StatusEvaluation(java.lang.Boolean decision,
+         java.lang.String reasonOfDecision)
+   {
+      this.decision = decision;
+      this.reasonOfDecision = reasonOfDecision;
    }
 
 }
