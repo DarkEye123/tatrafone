7cef251321a37f2e514f72bb1a5ef23909a2c554
Matej Lesko <mlesko@redhat.com>
2016-06-11 17:19:35 +0200

    NEW: rest services -- email

---
+++ rest-services/src/main/java/AppConfiguration.java
@@ -0,0 +1,8 @@
+import io.dropwizard.Configuration;
+
+/**
+ * @author darkeye
+ */
+public class AppConfiguration extends Configuration {
+
+}
---
+++ rest-services/src/main/java/EmailService.java
@@ -0,0 +1,28 @@
+import org.apache.commons.validator.routines.EmailValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author darkeye
+ */
+
+@Path("/")
+public class EmailService {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
+
+    @GET
+    @Path("email")
+    public Response checkDiskspace(@QueryParam("email") String email) {
+        logger.info("email=" + email);
+
+        EmailValidator emailValidator = EmailValidator.getInstance();
+        return emailValidator.isValid(email) ? Response.ok().build() :
+                Response.status(Response.Status.FORBIDDEN).build();
+    }
+}
