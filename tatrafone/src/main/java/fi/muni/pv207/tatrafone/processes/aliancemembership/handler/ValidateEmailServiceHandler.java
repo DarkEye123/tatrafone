f2443d252ef6e57cbc350ba6fe583f95d73af25c
Matej Lesko <mlesko@redhat.com>
2016-06-11 17:20:54 +0200

    Email validation service attempt with error

--- tatrafone/src/main/java/fi/muni/pv207/tatrafone/processes/aliancemembership/handler/ValidateEmailServiceHandler.java
+++ tatrafone/src/main/java/fi/muni/pv207/tatrafone/processes/aliancemembership/handler/ValidateEmailServiceHandler.java
@@ -1,19 +1,31 @@
 package fi.muni.pv207.tatrafone.processes.aliancemembership.handler;
 import fi.muni.pv207.tatrafone.processes.aliancemembership.ClientRequest;
 import fi.muni.pv207.tatrafone.validators.EmailValidator;
+import org.kie.api.runtime.process.EventListener;
+import org.kie.api.task.TaskEvent;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItemManager;
+import org.kie.api.runtime.KieRuntime;
+
+import java.util.HashMap;
 
 /**
- * Created by darkeye on 11/06/16.
+ * Created by darkeye on 12/06/16.
  */
 public class ValidateEmailServiceHandler implements WorkItemHandler {
 
     private EmailValidator validateEmail = new EmailValidator("9080", "/services/email");
 
     public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
-        //ClientRequest request = workItem.getParameter()
+        ClientRequest request = (ClientRequest) workItem.getParameter("ClientDescription");
+        Boolean validation_success = validateEmail.validate(request.getEmail());
+        HashMap<String, Object> resultMap = new HashMap<>();
+        resultMap.put("Result", validation_success);
+        workItemManager.completeWorkItem(workItem.getId(), resultMap);
+        if (validation_success.equals(Boolean.FALSE))
+            throw new Error("invalid_email");
+        return;
     }
 
     public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
