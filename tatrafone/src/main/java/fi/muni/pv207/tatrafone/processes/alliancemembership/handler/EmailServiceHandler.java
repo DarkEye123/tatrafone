1d421ca7df914964da6a2af03b7e3557ea56bfd5
Matej Lesko <mlesko@redhat.com>
2016-06-12 13:05:09 +0200

    EmailService reacts on body

--- tatrafone/src/main/java/fi/muni/pv207/tatrafone/processes/alliancemembership/handler/EmailServiceHandler.java
+++ tatrafone/src/main/java/fi/muni/pv207/tatrafone/processes/alliancemembership/handler/EmailServiceHandler.java
@@ -25,6 +25,9 @@ public class EmailServiceHandler extends EmailWorkItemHandler {
         } else {
             ClientRequest request = (ClientRequest) workItem.getParameter("i_client_request");
             workItem.getParameters().put("To", request.getEmail());
+            if (workItem.getParameters().get("Body").equals("")) {
+                workItem.getParameters().put("Body", request.getEvaluationOfPartnership());
+            }
 
             try {
                 Email e = createEmail(workItem, this.getConnection());
