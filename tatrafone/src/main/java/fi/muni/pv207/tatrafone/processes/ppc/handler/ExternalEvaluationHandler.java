26e99ff4ec43ef5868e54b940c32da5421b2fb3c
Matej Lesko <mlesko@redhat.com>
2016-06-16 04:13:46 +0200

    NEW: handlers for PPC campaign process

--- tatrafone/src/main/java/fi/muni/pv207/tatrafone/processes/ppc/handler/ExternalEvaluationHandler.java
+++ tatrafone/src/main/java/fi/muni/pv207/tatrafone/processes/ppc/handler/ExternalEvaluationHandler.java
@@ -1,6 +1,7 @@
 package fi.muni.pv207.tatrafone.processes.ppc.handler;
 
-import fi.muni.pv207.tatrafone.processes.connection_builders.analyzators.Analyser;
+import fi.muni.pv207.tatrafone.processes.connection_builders.evaluators.Evaluator;
+import fi.muni.pv207.tatrafone.processes.ppc.CampaignRequirement;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItemManager;
@@ -10,23 +11,31 @@ import java.util.HashMap;
 /**
  * Created by darkeye on 15/06/16.
  */
-public class ExternalAnalysisHandler implements WorkItemHandler {
+public class ExternalEvaluationHandler implements WorkItemHandler {
 
-    Analyser analyser = new Analyser();
+
+    Evaluator evaluator = new Evaluator();
 
     @Override
     public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
         String estimation = (String) workItem.getParameter("estimation");
+        CampaignRequirement campaignRequest = (CampaignRequirement) workItem.getParameter("Campaign");
+
         String[] values = estimation.split(":"); // message comes like 25.8:29.3
         Double cost = Double.parseDouble(values[0]);
         Double income = Double.parseDouble(values[1]);
+
+        campaignRequest.setCost(campaignRequest.getCost() + cost);
+        campaignRequest.setIncome(campaignRequest.getIncome() + income); // adding
+
         HashMap<String, Object> resultMap = new HashMap<>();
-        resultMap.put("Result", analyser.isCampaignSuccessful(cost, income));
+        resultMap.put("Result", evaluator.isDoingWell(cost, income));
+        resultMap.put("Campaign", campaignRequest);
         manager.completeWorkItem(workItem.getId(), resultMap);
     }
 
     @Override
     public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
-        
+
     }
 }
---
+++ tatrafone/src/main/java/fi/muni/pv207/tatrafone/processes/ppc/handler/PPCEmailHandler.java
@@ -0,0 +1,40 @@
+package fi.muni.pv207.tatrafone.processes.ppc.handler;
+
+import fi.muni.pv207.tatrafone.processes.alliancemembership.ClientRequest;
+import fi.muni.pv207.tatrafone.processes.ppc.CampaignRequirement;
+import org.jbpm.process.workitem.email.Email;
+import org.jbpm.process.workitem.email.EmailWorkItemHandler;
+import org.jbpm.process.workitem.email.SendHtml;
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemManager;
+
+import java.util.HashMap;
+
+/**
+ * @author darkeye
+ */
+public class PPCEmailHandler extends EmailWorkItemHandler {
+
+    public PPCEmailHandler(String host, String port, String userName, String password) {
+        super(host, port, userName, password);
+    }
+
+    @Override
+    public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+        if(this.getConnection() == null) {
+            throw new IllegalArgumentException("Connection not initialized for Email");
+        } else {
+            CampaignRequirement requirement = (CampaignRequirement) workItem.getParameter("Campaign");
+            workItem.getParameters().put("To", requirement.getEmail());
+            try {
+                Email e = createEmail(workItem, this.getConnection());
+                SendHtml.sendHtml(e, this.getDebugFlag(workItem));
+                if(manager != null) {
+                    manager.completeWorkItem(workItem.getId(), new HashMap<String, Object>());
+                }
+            } catch (Exception var4) {
+                this.handleException(var4);
+            }
+        }
+    }
+}
